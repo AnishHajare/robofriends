381da832c5bd52e719ab1d76fc85d1dfda0f5d9a
Anish Hajare <anish.hajare9@gmail.com>
2020-01-22 19:38:49 +0530

    Robofriends built using React;

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+//import Card from './Card';
+//import CardList from './CardList';
+import 'tachyons';
+//import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Bot Dan',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Bot Baburao',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Noob Bot Colonel',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Bot Zinc',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Bot Jabba',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Bot Dirti',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Hard Bot Goku',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+];
+
+export default robots;
